d243289690ac05fcf2787374b8711b4b2a250e5c
aalopezb <a.alexander.l.b@gmail.com>
2025-05-12 22:06:56 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/sumar", methods=["GET"])
+def sumar():
+    try:
+        # Obtener los parámetros 'a' y 'b' de la URL
+        a = float(request.args.get("a"))
+        b = float(request.args.get("b"))
+        resultado = a + b
+        return jsonify({"resultado": resultado})
+    except ValueError:
+        return jsonify({"error": "Por favor, ingrese números válidos."}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
